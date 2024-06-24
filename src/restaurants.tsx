cf0a98383d00d0301ebeda9360f2db32368978b8
cson357 <nntson@hotmail.com>
2024-06-25 02:21:32 +0300

    Updated columns for RestaurantList

--- src/restaurants.tsx
+++ src/restaurants.tsx
@@ -3,7 +3,8 @@ import { Datagrid, List, TextField } from "react-admin";
 export const RestaurantList = () => (
     <List>
         <Datagrid rowClick="edit">
-            <TextField source="id" />
+            <TextField source="properties.name" />
+            <TextField source="properties.opening_hours" />
         </Datagrid>
     </List>
 );
