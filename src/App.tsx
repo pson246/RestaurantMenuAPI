504f59007f69919b401d1baa53fc018df83e8626
cson357 <nntson@hotmail.com>
2024-06-26 11:53:43 +0300

    Added RestaurantEdit component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Admin, Resource } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { Dashboard } from "./dashboard/Dashboard";
-import { RestaurantList } from "./restaurants";
+import { RestaurantEdit, RestaurantList } from "./restaurants";
 
 export const App = () => (
   <Admin
     dataProvider={dataProvider}
     dashboard={Dashboard}>
-    <Resource name="restaurants" list={RestaurantList} />
+      <Resource name="restaurants" list={RestaurantList} edit={RestaurantEdit} />
   </Admin>
 );
--- src/restaurants.tsx
+++ src/restaurants.tsx
@@ -1,4 +1,4 @@
-import { Datagrid, List, TextField } from "react-admin";
+import { Datagrid, Edit, List, SimpleForm, TextField, TextInput } from "react-admin";
 
 export const RestaurantList = () => (
     <List>
@@ -7,4 +7,12 @@ export const RestaurantList = () => (
             <TextField source="properties.opening_hours" />
         </Datagrid>
     </List>
+);
+
+export const RestaurantEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="properties.opening_hours" />
+        </SimpleForm>
+    </Edit>
 );
