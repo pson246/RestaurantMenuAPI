920956192935b1c7976326aeccab37e08542de93
cson357 <nntson@hotmail.com>
2024-07-19 12:42:34 +0300

    Added edit button to RestaurantList, labels to RestaurantEdit

--- src/restaurants.tsx
+++ src/restaurants.tsx
@@ -1,6 +1,8 @@
 import {
     Datagrid,
     Edit,
+    EditButton,
+    Labeled,
     List,
     Show,
     SimpleForm,
@@ -14,23 +16,33 @@ export const RestaurantList = () => (
         <Datagrid rowClick="show">
             <TextField label="Name" source="properties.name" />
             <TextField label="Opening hours" source="properties.opening_hours" />
+            <EditButton />
         </Datagrid>
     </List>
 );
 
-export const RestaurantEdit = () => (
-    <Edit>
-        <SimpleForm>
-            <TextInput source="properties.opening_hours" />
-            <TextInput source="properties.foodItems" />
-        </SimpleForm>
-    </Edit>
-);
+export const RestaurantEdit = () => {
+    return (
+        <Edit title="Restaurant edit">
+            <SimpleForm>
+                <Labeled sx={{ marginBottom: "10px" }}>
+                    <TextField label="Name" source="properties.name" sx={{ marginTop: "5px" }} />
+                </Labeled>
+                <Labeled sx={{ marginBottom: "10px" }}>
+                    <TextField label="Opening hours" source="properties.opening_hours" sx={{ marginTop: "5px" }} />
+                </Labeled>
+                <Labeled>
+                    <TextInput label="Food items" source="properties.foodItems" />
+                </Labeled>                
+            </SimpleForm>
+        </Edit>
+    )
+};
 
 export const RestaurantShow = () => (
     <Show>
         <SimpleShowLayout>
-            <TextField source="properties.foodItems" />     
+            <TextField source="properties.foodItems" />
         </SimpleShowLayout>
     </Show>
 );
