a4a0263159457b6cb3bb782d8521248b908aa1fb
cson357 <nntson@hotmail.com>
2024-07-22 20:46:07 +0300

    Added environment variables to action workflow

--- src/dbConfig.ts
+++ src/dbConfig.ts
@@ -1,6 +1,6 @@
 export let dbConfig = {
-    "host": import.meta.env.VITE_REACT_APP_DB_HOST || process.env.REACT_APP_DB_HOST || "",
-    "authKey": import.meta.env.VITE_REACT_APP_DB_AUTH_KEY || process.env.REACT_APP_DB_AUTH_KEY || "",
+    "host": process.env.VITE_REACT_APP_DB_HOST || process.env.REACT_APP_DB_HOST || "",
+    "authKey": process.env.VITE_REACT_APP_DB_AUTH_KEY || process.env.REACT_APP_DB_AUTH_KEY || "",
     "databaseId": "Restaurant",
     "containerId": "EuropeFinlandHelsinkiRestaurants"
 };
