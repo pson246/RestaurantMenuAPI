47f8fa4f0c05ffd732bdffd7a26491876f3c192a
cson357 <nntson@hotmail.com>
2024-07-22 21:23:25 +0300

    Hidden restaurants for security reason

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { Admin, Resource } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
 import { Dashboard } from "./dashboard/Dashboard";
-import { RestaurantList, RestaurantShow } from "./restaurants";
 
 export const App = () => (
   <Admin
     dataProvider={dataProvider}
     authProvider={authProvider}
     dashboard={Dashboard}>
-    {/* <Resource name="restaurants" list={RestaurantList} edit={RestaurantEdit} show={RestaurantShow} /> */}
-    <Resource name="restaurants" list={RestaurantList} show={RestaurantShow} />
+    {/* <Resource name="restaurants" list={RestaurantList} edit={RestaurantEdit} show={RestaurantShow} /> */}    
+    <Resource name="restaurants" />
   </Admin>
 );
