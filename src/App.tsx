478cec9f6afe8b9f3077491b49c55bc0ae271eb0
cson357 <nntson@hotmail.com>
2024-07-22 15:53:48 +0300

    Added authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Admin, Resource } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { authProvider } from "./authProvider";
 import { Dashboard } from "./dashboard/Dashboard";
 import { RestaurantEdit, RestaurantList, RestaurantShow } from "./restaurants";
 
 export const App = () => (
   <Admin
     dataProvider={dataProvider}
+    authProvider={authProvider}
     dashboard={Dashboard}>
     <Resource name="restaurants" list={RestaurantList} edit={RestaurantEdit} show={RestaurantShow} />
   </Admin>
