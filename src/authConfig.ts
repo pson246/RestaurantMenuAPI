0feb36e798d50caeb92d7d3bdb1b5b96518041ae
cson357 <nntson@hotmail.com>
2024-07-24 17:43:02 +0300

    Updated auth callback to fix redirect error

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -8,7 +8,7 @@ export const msalConfig: Configuration = {
     // Full directory URL, in the form of https://login.microsoftonline.com/<tenant-id>
     authority: import.meta.env.VITE_MSAL_AUTHORITY || process.env.VITE_MSAL_AUTHORITY || "",
     // Full redirect URL, in form of http://localhost:8080/auth-callback
-    redirectUri: `${import.meta.env.VITE_APP_BASE_URI}/` || `${process.env.VITE_APP_BASE_URI}/` || "",
+    redirectUri: `${import.meta.env.VITE_APP_BASE_URI}/auth-callback` || `${process.env.VITE_APP_BASE_URI}/auth-callback` || "",
     // We need to disable this feature because it is already handled by react-admin, and would otherwise conflict
     navigateToLoginRequestUrl: false,
   },
