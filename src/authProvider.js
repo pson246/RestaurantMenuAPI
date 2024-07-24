c54c87fea4c225c6e219b0b367cd52dc470253d0
cson357 <nntson@hotmail.com>
2024-07-24 14:25:21 +0300

    Updated to Active Directory authentication

---
+++ src/authProvider.js
@@ -0,0 +1,10 @@
+import { msalAuthProvider } from "ra-auth-msal";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "./authConfig";
+
+const myMSALObj = new PublicClientApplication(msalConfig);
+
+await myMSALObj.initialize();
+
+export const authProvider = msalAuthProvider({ msalInstance: myMSALObj });
+
