c9bb051cbde0700db90a966c85060fe8b4690f34
cson357 <nntson@hotmail.com>
2024-07-29 18:13:02 +0300

    Changed Http client, filtered white space attempt 1

--- api/EuropeFinlandHelsinkiFetchRestaurantsMenuHttp/index.js
+++ api/EuropeFinlandHelsinkiFetchRestaurantsMenuHttp/index.js
@@ -1,26 +1,47 @@
-const superagent = require("superagent");
+const axios = require("axios");
 const cheerio = require("cheerio");
-const EUROPE_FINLAND_HELSINKI_PIKKU_RANSKA_LOUNAS_PAGE_URL = "http://www.pikkuranska.com/lounas.htm";
 // lounas menu === lunch menu in Finnish
 var pikkuRanskaLounasMenu = "";
+const EUROPE_FINLAND_HELSINKI_PIKKU_RANSKA_LOUNAS_PAGE_URL = "http://www.pikkuranska.com/lounas.htm";
+
+const hasOneTypeOfCharacter = (str) => {
+    const characters = str.split("");
+    const set = new Set(characters);
+    // one type of character + new line character
+    return (set.size === 2);
+};
 
 const fetchPikkuRanskaLounasMenu = async (pageUrl) => {    
     try {
-        const response = await superagent.get(pageUrl);        
-        const $ = cheerio.load(response.text);
-        const menuItems = [];
-        $("p.MsoNormal b i span").map((i, element) => {                
-            const menuItem = $(element).text().replace(/\uFFFD/g, "").trim();
-            menuItems.push(menuItem);            
+        const response = await axios.request({
+            method: "GET",
+            url: pageUrl,
+            responseType: "arraybuffer",
+            responseEncoding: "binary"
         });
-        pikkuRanskaLounasMenu = menuItems.join();
+        
+        const responseText = response.data.toString("binary");   
+        const $ = cheerio.load(responseText, {decodeEntities: true});
+        const menuItems = [];
+        $("p.MsoNormal b i span").map((i, element) => {            
+            var menuItem = $(element).text();
+            if (hasOneTypeOfCharacter(menuItem)) {
+                menuItem = menuItem.replace(/\s/g, '');
+            } else {
+                menuItem = menuItem.replace(/\s/g, ' ').trim();
+            }            
+            if (menuItem !== '') {
+                menuItems.push(menuItem);
+            }                        
+        });                
+        pikkuRanskaLounasMenu = menuItems.join("");        
     } catch (error) {
         console.error("Error fetching lounas menu for restaurant Europe.Finland.Helsinki.PikkuRanska: ", error);
     }    
 };
 
-module.exports = async function (context, req) {
-    await fetchPikkuRanskaLounasMenu(EUROPE_FINLAND_HELSINKI_PIKKU_RANSKA_LOUNAS_PAGE_URL);    
+module.exports = async function (context, req) {    
+    await fetchPikkuRanskaLounasMenu(EUROPE_FINLAND_HELSINKI_PIKKU_RANSKA_LOUNAS_PAGE_URL);
     context.res.json({
         "Europe.Finland.Helsinki.PikkuRanska": {
             "lounasMenu": pikkuRanskaLounasMenu
